e16ae420920fabe18ea31499a27d38a70ea72833
iakud <kaikai87111@gmail.com>
2018-06-13 23:46:53 +0800

    vector

---
+++ sstl_vector.h
@@ -0,0 +1,14 @@
+#ifndef SSTL_VECTOR_H
+#define SSTL_VECTOR_H
+
+namespace sstl {
+
+template<typename T>
+class vector {
+	using value_type = T;
+	using pointer = T*;
+}; // end class vector
+
+} // end namespace sstl
+
+#endif SSTL_VECTOR_H
